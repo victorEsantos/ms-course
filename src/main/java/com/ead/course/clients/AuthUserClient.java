1f224041a8177a67d3e7f3558d5875dfd81ca9a6
victor <victor.eduardo.sntos@gmail.com>
2021-12-09 22:05:01 -0300

    validacao user

--- src/main/java/com/ead/course/clients/AuthUserClient.java
+++ src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 
 @Component
 @Log4j2
-public class CourseClient {
+public class AuthUserClient {
 
     @Autowired
     RestTemplate restTemplate;
@@ -47,6 +47,12 @@ public class CourseClient {
         }
 
         return new PageImpl<>(searchResult);
+    }
+
+    public ResponseEntity<UserDto> getOneUserById(UUID id) {
+        var url = REQUEST_URL_AUTHUSER + "/users/" + id;
+
+        return restTemplate.exchange(url, HttpMethod.GET, null, UserDto.class);
     }
 
 }
--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -1,8 +1,9 @@
 package com.ead.course.controllers;
 
-import com.ead.course.clients.CourseClient;
+import com.ead.course.clients.AuthUserClient;
 import com.ead.course.dtos.SubscriptionDto;
 import com.ead.course.dtos.UserDto;
+import com.ead.course.enums.UserStatus;
 import com.ead.course.models.Course;
 import com.ead.course.services.CourseService;
 import com.ead.course.services.CourseUserSerivice;
@@ -14,6 +15,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -21,10 +23,10 @@ import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-public class CourseUSerController {
+public class CourseUserController {
 
     @Autowired
-    CourseClient courseClient;
+    AuthUserClient authUserClient;
 
     @Autowired
     CourseService courseService;
@@ -35,13 +37,15 @@ public class CourseUSerController {
     @GetMapping("/courses/{courseId}/users")
     public ResponseEntity<Page<UserDto>> getAllUsersByCourse(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
                                                              @PathVariable(value = "courseId") UUID courseId){
-        return ResponseEntity.ok().body(courseClient.getAllUsersByCourse(courseId, pageable));
+        return ResponseEntity.ok().body(authUserClient.getAllUsersByCourse(courseId, pageable));
 
     }
 
     @PostMapping("/courses/{courseId}/users/subscription")
     public ResponseEntity<Object> saveSubscriptionUserInCourse(@PathVariable(value = "courseId") UUID courseId,
                                                                @RequestBody @Valid SubscriptionDto dto){
+
+        ResponseEntity<UserDto> responseUSer;
 
         Optional<Course> courseModelOptional = courseService.findById(courseId);
         if (!courseModelOptional.isPresent()) {
@@ -51,10 +55,22 @@ public class CourseUSerController {
         if(courseUserSerivice.existsByCourseAndUserId(courseModelOptional.get(), dto.getUserId())){
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Subscription already exists.");
         }
-        //verificacao de user
+
+
+        try {
+            responseUSer = authUserClient.getOneUserById(dto.getUserId());
+
+            if(responseUSer.getBody().getUserStatus().equals(UserStatus.BLOCKED)){
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: User is blocked.");
+            }
+        } catch (HttpStatusCodeException e) {
+            if(e.getStatusCode().equals(HttpStatus.NOT_FOUND)){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found.");
+            }
+        }
 
         var courseUser = courseUserSerivice.save(courseModelOptional.get().convertToCourseUser(dto.getUserId()));
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("Subscription created successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseUser);
     }
 }
